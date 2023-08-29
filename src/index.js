7bf4d6438e2fcfd1d67a68d0c1607c92a283e8d6
Jonatas Rodolfo Pereira dos Santos <rodolfojonatas@outlook.com>
2023-08-28 22:08:41 -0300

    feat: finishing to-dos functional requirements

--- src/index.js
+++ src/index.js
@@ -18,7 +18,6 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
   const { username } = request.headers;
   const user = users.find(
     user => user.username === username
@@ -33,8 +32,16 @@ function checksExistsUserAccount(request, response, next) {
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
   const { name, username } = request.body;
+
+  const userExists = users.some(user => user.username === username);
+
+  if(userExists) {
+    return response.status(400).json({
+      error: 'Mensagem do erro'
+    });
+  }
+
   const user = {
     id: uuidv4(),
     name: name,
@@ -46,23 +53,81 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  return response.status(200).json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  const todo = {
+    id: uuidv4(),
+    title: title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+  user.todos.push(todo)
+  return response.status(201).json(todo);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+  const { title, deadline } = request.body;
+
+  const todo = user.todos.find(
+    (todo) => todo.id === id
+  );
+
+  if (!todo) {
+    return response.status(404).json({
+      error: 'Mensagem do erro'
+    });
+  }
+
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.json(todo);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find(
+    (todo) => todo.id === id
+  );
+
+  if (!todo) {
+    return response.status(404).json({
+      error: 'Mensagem do erro'
+    });
+  }
+
+  todo.done = true;
+
+  return response.json(todo);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find(
+    (todo) => todo.id === id
+  );
+
+  if (!todo) {
+    return response.status(404).json({
+      error: 'Mensagem do erro'
+    });
+  }
+
+  user.todos.splice(todo.id, 1);
+  return response.status(204).json(user.todos);
 });
 
 module.exports = app;
